a48ba00bc7703508c52940c3fe14cf805e4945bc
homelessking79 <52943875+homelessking79@users.noreply.github.com>
2023-05-11 16:31:48 +0700

    Update ai.py

--- AI_IOT/ai.py
+++ AI_IOT/ai.py
@@ -2,6 +2,10 @@ import cv2
 import torch
 import json
 import IOT
+import board,busio
+import numpy as np
+import adafruit_mlx90640
+import matplotlib.pyplot as plt
 
 class AICam:
     def __init__(self):
@@ -76,7 +80,45 @@ class AICam:
 
     def processImages(self):
         # TODO: process camera images and inferred camera images and ouput isFire
-        pass
+        print("Initializing MLX90640")
+        i2c = busio.I2C(board.SCL, board.SDA, frequency=800000) # setup I2C
+        mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
+        mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
+        mlx_shape = (24,32)
+        print("Initialized")
+
+        fire_threshold = 90 # temperature threshold for fire detection
+
+        # setup the figure for plotting
+        plt.ion() # enables interactive plotting
+        fig,ax = plt.subplots(figsize=(12,7))
+        therm1 = ax.imshow(np.zeros(mlx_shape),vmin=0,vmax=60) #start plot with zeros
+        cbar = fig.colorbar(therm1) # setup colorbar for temps
+        cbar.set_label('Temperature [$^{\circ}$C]',fontsize=14) # colorbar label
+
+        frame = np.zeros((24*32,)) # setup array for storing all 768 temperatures
+        # t_array = []
+            # t1 = time.monotonic()
+        try:
+            mlx.getFrame(frame) # read MLX temperatures into frame var
+            data_array = (np.reshape(frame,mlx_shape)) # reshape to 24x32
+            therm1.set_data(np.fliplr(data_array)) # flip left to right
+            therm1.set_clim(vmin=np.min(data_array),vmax=np.max(data_array)) # set bounds
+            cbar.update_normal(therm1) # update colorbar range
+
+            fire_count = np.sum(data_array > fire_threshold) # count the number of pixels above the fire threshold
+            if (fire_count > (0.5 * mlx_shape[0] * mlx_shape[1]) and self.isFire): # if more than half of the pixels exceed the threshold
+                self.isFire = True
+            else:
+                self.isFire = False
+
+            plt.title(f"Max Temp: {np.max(data_array):.1f}C")
+            plt.pause(0.001) # required
+            #fig.savefig('mlx90640_test_fliplr.png',dpi=300,facecolor='#FCFCFC', bbox_inches='tight') # comment out to speed up
+            # t_array.append(time.monotonic()-t1)
+            # print('Sample Rate: {0:2.1f}fps'.format(len(t_array)/np.sum(t_array)))
+        except ValueError:
+            print('Error reading thermal camera')
 
     def buildJson(self):
         # check if any data have been read
